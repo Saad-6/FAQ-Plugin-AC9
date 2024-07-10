6326c2665d844281c73c518dbc6110b885c83337
Saad-6 <131909921+Saad-6@users.noreply.github.com>
2024-07-10 15:43:58 +0500

    Removed files specified in .gitignore

--- Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("FAQPlugin")]
-[assembly: AssemblyDescription("Example FAQPLugin for AbleCommerce.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyVersion("9.0.*")]
